2e89ebf7f7911cbd1613f6226b25858d91c299db
Łukasz Matuszewski <lumat@ukaszs-MacBook-Pro.local>
2025-02-05 11:02:13 +0100

    fetch worklogs in consumer

--- src/consumer.js
+++ src/consumer.js
@@ -1,8 +1,21 @@
 import Resolver from "@forge/resolver";
+import api, { route } from '@forge/api';
+
 const resolver = new Resolver();
 
 resolver.define("event-listener", async ({ payload, context }) => {
   console.log(payload);
+
+  const result = await api
+      .asApp()
+      .requestJira(route`rest/api/3/worklog/updated?since=0`);
+
+  const status = result.status;
+
+  console.log(status);
+
+  const data = await result.json();
+  console.log('==data', data);
 });
 
 export const handler = resolver.getDefinitions();
--- src/resolvers/trigger.js
+++ src/resolvers/trigger.js
@@ -5,5 +5,5 @@ const queue = new Queue({ key: 'worklog-fetch' });
 export const onInstall = async (event) => {
   console.log('trigger event', event);
 
-  await queue.push({something: 1111}, {delayInSeconds: 30});
+  await queue.push({something: 1111}, {delayInSeconds: 5});
 }
