fc09868c339738652e71a03677c63a3636b74949
Łukasz Matuszewski <lumat@ukaszs-MacBook-Pro.local>
2025-02-05 16:57:07 +0100

    search worklogs by started_at

--- src/frontend/index.jsx
+++ src/frontend/index.jsx
@@ -1,24 +1,65 @@
 import React, { useEffect, useState } from 'react';
-import ForgeReconciler, {Button, Text} from '@forge/react';
+import ForgeReconciler, {Button, Text, DatePicker, Inline, DynamicTable} from '@forge/react';
 import { invoke } from '@forge/bridge';
 
+const getHeader = () => (
+    { cells: [
+            {
+                key: 'Id',
+                content: <Text>Id</Text>,
+                width: 16,
+            },
+            {
+                key: 'startAt',
+                content: <Text>Start at</Text>,
+                width: 20,
+            }
+        ]});
+
 const App = () => {
+  const [startAt, setStartAt] = useState('2024-02-05 00:00:00');
+  const [endAt, setEndAt] = useState('2025-02-06 00:00:00');
   const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
   useEffect(() => {
-    invoke('getWorklogs').then(setData);
+      setIsLoading(true);
+    invoke('getWorklogs').then(setData).then(() => setIsLoading(false));
   }, []);
+
+  const handleSearch = () => {
+      setIsLoading(true);
+      invoke('searchWorklogs', { startAt, endAt }).then(setData).then(() => setIsLoading(false));
+  }
 
   if (!data) return 'Loading...'
 
+    const rows = data.results.rows.map((worklog) => ({ key: worklog.worklog_id, cells: [
+            {
+                key: worklog.worklog_id,
+                content: <Text>{worklog.worklog_id}</Text>,
+            },
+            {
+                key: worklog.started_at,
+                content: <Text>{worklog.started_at}</Text>,
+            }
+        ] }));
+
   return (
     <>
+      <Inline alignBlock="center" space="space.100">
+        <Text>Start at</Text>
+        <DatePicker value={startAt} onChange={setStartAt} maxDate={endAt}/>
+        <Text>End at</Text>
+        <DatePicker value={endAt} onChange={setEndAt} minDate={startAt} />
+        <Button onClick={handleSearch}>Search</Button>
+      </Inline>
+
       <Text>Count: {data.count}</Text>
       <Button onClick={() => invoke('fetchWorklogs')}>Fetch</Button>
       <Button onClick={() => invoke('runMigration')}>Migrate</Button>
 
-      {data.results.rows.map((worklog) => {
-        return <Text>{worklog.worklog_id} {worklog.started_at}</Text>;
-      })}
+      <DynamicTable rows={rows} head={getHeader()} isLoading={isLoading}/>
     </>
   );
 };
--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -16,6 +16,19 @@ resolver.define('getWorklogs', async (req) => {
   return { results, count: countResponse.rows[0]['COUNT(*)'] };
 });
 
+resolver.define('searchWorklogs', async (req) => {
+  const { startAt, endAt } = req.payload;
+
+  const results = await sql
+      .prepare(`SELECT * FROM Worklogs WHERE started_at BETWEEN '${startAt}' AND '${endAt}';`)
+      .execute();
+  const countResponse = await sql
+      .prepare(`SELECT COUNT(*) FROM Worklogs WHERE started_at BETWEEN '${startAt}' AND '${endAt}';`)
+      .execute();
+
+  return { results, count: countResponse.rows[0]['COUNT(*)'] };
+});
+
 resolver.define('runMigration', async (req) => {
   await runSchemaMigration();
 
